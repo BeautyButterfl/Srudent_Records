ebf4c2ca74bec5845e7a49a2aed76ee1f804ad31
sheanne_cruz@rcc.edu.ph <BeautyButterfl>
2024-05-15 10:36:32 +0800

    finals

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AIMS</title>
+
+</head>
+
+    <style>
+        body {background-color: #ADD8E6}
+        h1 {text-align: center;
+            font-family: Verdana
+        }
+        p {text-align: center;
+           font-family: Verdana
+        }
+        li {text-align: center;
+            font-family: Verdana
+        }
+        img {display: block;
+             margin-left: auto;
+             margin-right: auto;
+             border:1px solid black
+            }
+        
+    </style>
+
+    <body>
+    <h1>Welcome to Student Records</h1>
+    <p>Please select an option:</p>
+
+        <li><a href="input_form.php">Input New Record</a></li>
+        <li><a href="delete.php">Delete Record</a></li>
+    
+    </body>
+</html>
---
+++ input_form.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+   
+</head>
+<body>
+
+<form action="student_info.php" method="POST">
+Student Name:    <input type="text" name="name"><br>
+Age:             <input type="text" name="age"><br>
+Grade:           <input type="text" name="grade"><br>
+<input type="submit">
+</form>
+<p><a href="home.php">Back to Home</a></p>
+</body>
+</html>
